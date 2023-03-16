59b54f384920afaa833892ddb00b00bb7a44d0a0
Hamid Ehsani <75583986+hamidehsani54@users.noreply.github.com>
2023-03-16 14:49:45 +0100

    Update ConnectionToServer.java
    Some updates. we can send file to server. 
    /Todo: 
    it should support the jpg format.

--- Local/app/src/main/java/com/example/drone_interactor/ConnectionToServer.java
+++ Local/app/src/main/java/com/example/drone_interactor/ConnectionToServer.java
@@ -1,10 +1,8 @@
 import java.io.*;
 import java.net.*;
-import java.util.Base64;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import javax.print.attribute.standard.PrinterMakeAndModel;
+
 
 
 public class ConnectionToServer {
@@ -15,38 +13,36 @@ public class ConnectionToServer {
         
      
         try (// Establish a connection to the server 192.168.101.141 20276
-        Socket socket = new Socket("localhost", 20276)) {
+        Socket socket = new Socket("192.168.101.141", 20276)) {
             // Create input/output streams for communication with the server
             // Send the verification code to the server
-            //String[] command = {"no help", "sensor friend", "phone friend"};
-
-            
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            //BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));       
+                       
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);                  
             out.println("no help");
+
             TimeUnit.SECONDS.sleep(2);
             PrintWriter out1 = new PrintWriter(socket.getOutputStream(), true);
-            //BufferedReader in1 = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            //out1.println("sensor friend");
-            //TimeUnit.SECONDS.sleep(5);
+
+          
             TimeUnit.SECONDS.sleep(2);
             PrintWriter out2 = new PrintWriter(socket.getOutputStream(), true);
-            //BufferedReader in2 = new BufferedReader(new InputStreamReader(socket.getInputStream()));       
+
+                 
             out2.println("phone friend");
             TimeUnit.SECONDS.sleep(2);
 
             try{
-                    //dataInputStream = new DataInputStream(socket.getInputStream());
+                    
                     dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
                     sendFile("/Users/Hamid Ehsani/Desktop/2.jpeg");
                     
                     System.out.printf("The file has been sent to the server ");
-                   
+                    //TimeUnit.SECONDS.sleep(100);
                 
                 }
             catch (Exception e){
-                    System.out.println("OOOOOOOhhhhhh!!");
+                    System.out.println("OOOOOOOhhhhhh I know what is fel!!");
                 }
         }
         
@@ -54,20 +50,20 @@ public class ConnectionToServer {
 
 
     private static void sendFile(String path) throws Exception{
-        int bytes = 0;
+        
         File file = new File(path);
         FileInputStream fileInputStream = new FileInputStream(file);
 
         // send file size
-        String str = "img aaa.jpg;" + file;
-        System.out.println(file.length());
-        //dataOutputStream.write(str.getBytes());
+        String str = "img Fabian.jpg;" + file.length();
+       
+        dataOutputStream.write(str.getBytes());
         TimeUnit.SECONDS.sleep(1);
 
-        // break file into chunks
-        //byte[] buffer = new byte[1024];
-        
+   
         for(int a=0; a<file.length(); a++) {
+        
+            
             if(file.length()==1024){
                 TimeUnit.MILLISECONDS.sleep(5);
                 dataOutputStream.write('a');
@@ -75,21 +71,6 @@ public class ConnectionToServer {
             dataOutputStream.write('a');
             
         }
-        /*
-        for(int a=0; a<=file.length(); a++) {
-            dataOutputStream.write('b');
-        }
-
-        // send file size
-        dataOutputStream.writeLong(file.length());  
-        // break file into chunks
-        byte[] buffer = new byte[4*1024];
-        while ((bytes=fileInputStream.read(buffer))!=-1){
-            dataOutputStream.write(buffer,0,bytes);
-            dataOutputStream.flush();
-        }
-
         fileInputStream.close();
-    }
-     */
+
 }}
